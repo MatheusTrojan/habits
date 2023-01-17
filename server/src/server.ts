da240711f06ab2ebdc08583ea388c961cfde90e9
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-17 15:14:19 -0300

    configura o prisma para banco de dados SQLite

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,20 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client"; 
 
 const app = Fastify();
+const prisma = new PrismaClient();
 
-app.get("/", () => {
-    return "olá mundo"
+app.get("/hello", async () => {
+
+    const habits = await prisma.habit.findMany({
+
+    })
+
+    return habits
 })
 
 app.listen({
-    port:3333
+    port: 3333,
+}).then(() => {
+    console.log("HTTP Server running")
 })
