7199b75130d7073b5c8bbd4bde11be7e80242c86
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-17 15:24:06 -0300

    instala e registra o cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,11 @@
 import Fastify from "fastify";
+import cors from "@fastify/cors"
 import { PrismaClient } from "@prisma/client"; 
 
 const app = Fastify();
 const prisma = new PrismaClient();
+
+app.register(cors)
 
 app.get("/hello", async () => {
 
