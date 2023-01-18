66610e881fd551deaa3a6c2e6e32eb817b52d0ab
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-18 16:12:42 -0300

    cria componente dos dias do ano

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 import "./styles/global.css";
 
 // import Habit from "./components/Habit"
@@ -9,6 +10,7 @@ export function App() {
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
          <Header />
+         <SummaryTable />
       </div>
     </div>
 
---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,9 @@
+// interface HabitDayProps {
+//     completed: number
+// }
+
+export default function HabitDay() {
+  return (
+    <div className="w-10 h-10 bh-zinc-900 border-2 border-zinc-800 rounded-lg "></div>
+  )
+}
