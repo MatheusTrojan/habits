3a541aaac03b5fa58d9777a382e5413e4b91457e
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-18 14:49:35 -0300

    reorganiza arquivos de rotas

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,12 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors"
-import { PrismaClient } from "@prisma/client"; 
+import { appRoutes } from "./routes";
+
 
 const app = Fastify();
-const prisma = new PrismaClient();
 
 app.register(cors)
-
-app.get("/hello", async () => {
-
-    const habits = await prisma.habit.findMany({
-
-    })
-
-    return habits
-})
+app.register(appRoutes)
 
 app.listen({
     port: 3333,
