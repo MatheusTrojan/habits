bf13d130145a6ce1d23456d1ee5cd96981f2a80c
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-18 18:35:33 -0300

    corrige key da view do placeholder

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -47,6 +47,7 @@ export function Home() {
                         .from({ length: amountOfDaysToFill })
                         .map((_, index) => (
                             <View 
+                                key={index}
                                 className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
                                 style={{ width: SQUARE_SIZE, height: SQUARE_SIZE}}
                             />
