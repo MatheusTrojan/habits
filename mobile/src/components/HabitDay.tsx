bc8a9ea51ae724c771bdf28f2dcb57742385630b
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-18 18:32:16 -0300

    finaliza interface inicial da home

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -11,6 +11,7 @@ export function HabitDay() {
     <TouchableOpacity 
         className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800"
         style={{ width: SQUARE_SIZE, height: SQUARE_SIZE}}
+        activeOpacity={0.7}
     />
   )
 }
--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,9 +1,14 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+
+import { generateDatesFromYearBeginning } from "../utils/generate-dates-from-year-beginning"
 
 import { HabitDay, SQUARE_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
 
 const weekDay = ["D", "S", "T", "Q", "Q", "S", "S"];
+const datesFromYearStart = generateDatesFromYearBeginning();
+const minimumSummaryDatesSize = 18 * 5;
+const amountOfDaysToFill = minimumSummaryDatesSize - datesFromYearStart.length
 
 export function Home() {
     return (
@@ -24,7 +29,31 @@ export function Home() {
                 }
             </View>
 
-            <HabitDay />
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 100 }}            
+            >
+                <View className="flex-row flex-wrap">
+                    {
+                        datesFromYearStart.map(date => (
+                            <HabitDay 
+                            key={date.toISOString()}    
+                            />
+                        ))
+                    }
+
+                    {
+                        amountOfDaysToFill > 0 && Array
+                        .from({ length: amountOfDaysToFill })
+                        .map((_, index) => (
+                            <View 
+                                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-40"
+                                style={{ width: SQUARE_SIZE, height: SQUARE_SIZE}}
+                            />
+                        ))
+                    }
+                </View>
+            </ScrollView>
         </View>
     )
 }
