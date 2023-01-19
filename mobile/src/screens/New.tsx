99ca8d4cce8d9f63c6a4edf0336357c90ec13de2
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-19 15:23:55 -0300

    define as tipagens das rotas

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -1,9 +1,14 @@
-import { View } from "react-native";
+import { View, ScrollView } from "react-native";
+import { BackButton } from "../components/BackButton";
 
 export function New() {
-  return (
-    <View className="flex-1 bg-background px-8 pt-16">
+	return (
+		<View className="flex-1 bg-background px-8 pt-16">
+			<ScrollView showsVerticalScrollIndicator={false}>
+			
+			<BackButton />
 
-    </View>
-  )
+			</ScrollView>
+		</View>
+	)
 }
