4c8424e5f908d4838238ca1477b953c2527c9d51
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-19 15:11:15 -0300

    cria as rotas da aplicação

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -9,7 +9,7 @@ import {
 } from '@expo-google-fonts/inter';
 
 import { Loading } from './src/components/Loading';
-import { Home } from './src/screens/Home';
+import { Routes } from "./src/routes";
 
 export default function App() {
 
@@ -28,7 +28,7 @@ export default function App() {
 
   return (
     <>
-      <Home />
+      <Routes />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
     </>
   );
