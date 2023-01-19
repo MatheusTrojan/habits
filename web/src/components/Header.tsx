e4ece87d09e7be7861b30a5eed933f319842d8c2
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-19 11:54:29 -0300

    cria formulario do modal

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -2,6 +2,7 @@ import { Plus, X } from "phosphor-react";
 import * as Dialog from '@radix-ui/react-dialog';
 
 import logoImage from "../assets/logo.svg";
+import { NewHabitForm } from "./NewHabitForm";
 
 export function Header() {
 
@@ -10,6 +11,7 @@ export function Header() {
 			<img src={logoImage} alt="logo Habits" />
 
 			<Dialog.Root>		
+
 				<Dialog.Trigger				
 					type="button"
 					className="border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300"
@@ -21,15 +23,22 @@ export function Header() {
 				<Dialog.Portal>
 					<Dialog.Overlay className="w-screen h-screen bg-black/80 fixed inset-0"/>
 
-					<Dialog.Content 
-						className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
-					>
+					<Dialog.Content className="absolute p-10 bg-zinc-900 rounded-2xl w-full max-w-md top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+
 						<Dialog.Close className="absolute right-6 top-6 text-zinc-400 hover:text-zinc-200">
 							<X size={24} aria-label="Fechar"/>
 						</Dialog.Close>
-						Conteudo Modal
+						
+						<Dialog.Title className="text-3xl leading-tight font-extrabold">
+							Criar Hábito
+						</Dialog.Title>
+
+						<NewHabitForm />
+
 					</Dialog.Content>
+
 				</Dialog.Portal>
+
 			</Dialog.Root>
 		</div>
 	);
