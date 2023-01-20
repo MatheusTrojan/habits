0ca64ee8afcef860c45f7e2d55645786e6bd86bd
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-20 11:15:14 -0300

    adiciona estilização ao checkbox

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -38,13 +38,13 @@ export default function HabitDay({ amount, completed }: HabitDayProps) {
 						<Checkbox.Root
 							className="flex items-center gap-3 group"
 						>
-							<div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500">
+							<div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
 								<Checkbox.Indicator>
 									<Check size={20} className="text-white"/>
 								</Checkbox.Indicator>
 							</div>
 
-							<span className="font-semibold text-xl text-white leading-tight">
+							<span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
 								Beber 2L de Água
 							</span>
 						</Checkbox.Root>
