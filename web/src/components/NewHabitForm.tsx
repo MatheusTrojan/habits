3fb671b23caedb37219b43fd80cc8820f2930536
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-22 23:21:54 -0300

    adiciona dias da semana ao formulário

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,6 +1,16 @@
 import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
 
+const availableWeekDays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado"
+]
+
 export function NewHabitForm() {
     return (
         <form className="w-full flex flex-col mt-6">
@@ -21,17 +31,21 @@ export function NewHabitForm() {
             </label>
 
             <div className="flex flex-col gap-2 mt-3">
-                <Checkbox.Root className="flex items-center gap-3 group">
-                <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
-                    <Checkbox.Indicator>
-                    <Check size={20} className="text-white" />
-                    </Checkbox.Indicator>
-                </div>
+                {availableWeekDays.map(weekDay => {
+                    return (
+                        <Checkbox.Root key={weekDay} className="flex items-center gap-3 group">
+                        <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                            <Checkbox.Indicator>
+                            <Check size={20} className="text-white" />
+                            </Checkbox.Indicator>
+                        </div>
 
-                <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
-                    Beber 2L de Água
-                </span>
-                </Checkbox.Root>
+                        <span className="text-white leading-tight">
+                            {weekDay}
+                        </span>
+                        </Checkbox.Root>
+                    )
+                })}
             </div>
 
             <button
