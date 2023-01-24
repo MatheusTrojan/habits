34e43c6a048a06f37efb89e1ac18d92a0078bdfe
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-24 09:35:18 -0300

    inicia comunicação com backend - criação de habitos

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -1,6 +1,7 @@
 import * as Checkbox from "@radix-ui/react-checkbox";
 import { Check } from "phosphor-react";
 import {FormEvent, useState }  from 'react';
+import { api } from "../lib/axios";
 
 const availableWeekDays = [
     "Domingo",
@@ -17,10 +18,22 @@ export function NewHabitForm() {
     const [title, setTitle] = useState("")
     const [weekDays, setWeekDays] = useState<number[]>([])
 
-    function createNewHabit(event: FormEvent) {
+    async function createNewHabit(event: FormEvent) {
         event.preventDefault()
         
-        console.log(title, weekDays)
+        if (!title || weekDays.length === 0) {
+            return
+        } 
+
+        await api.post("habits", {
+            title, 
+            weekDays
+        })
+
+        setTitle("")
+        setWeekDays([])
+
+        alert ("Hábito criado com sucesso!")
     }
 
     function handleToggleWeekDay(weekDay: number) {
@@ -47,6 +60,7 @@ export function NewHabitForm() {
                 placeholder="ex.: Exercícios, dormir bem, etc"
                 autoFocus
                 className="p-4 rounded-lg mt-3 bg-zinc-800 text-white placeholder:text-zinc-400"
+                value={title}
                 onChange={event => setTitle(event.target.value)}
             />
 
@@ -60,6 +74,7 @@ export function NewHabitForm() {
                         <Checkbox.Root 
                             key={weekDay} 
                             className="flex items-center gap-3 group"
+                            checked={weekDays.includes(index)}
                             onCheckedChange={() => handleToggleWeekDay(index)}
                         >
                         <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
