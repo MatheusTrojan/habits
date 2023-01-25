06fe8a222c25abdc5adc5a8d6df0a34823520496
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-25 13:39:10 -0300

    corrige o host no server para rodar o mobile direito

--- mobile/src/screens/New.tsx
+++ mobile/src/screens/New.tsx
@@ -24,7 +24,7 @@ export function New() {
 	async function handleCreateNewHabit() {
 		try {
 			if (!title.trim() || weekDays.length === 0){
-				Alert.alert("Novo Hábito", "Informe o nome do hábito e escolha a periodicidade!")
+				return Alert.alert("Novo Hábito", "Informe o nome do hábito e escolha a periodicidade!")
 			}
 
 			await api.post("/habits", { title, weekDays })
--- server/src/server.ts
+++ server/src/server.ts
@@ -10,6 +10,7 @@ app.register(appRoutes)
 
 app.listen({
     port: 3333,
+    host: "0.0.0.0",
 }).then(() => {
     console.log("HTTP Server running")
 })
