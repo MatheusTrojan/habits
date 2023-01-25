b071265b290f848d52f48a7cc1062286f101e45a
Matheus Trojan <matheustrojan23@gmail.com>
2023-01-25 08:53:35 -0300

    cria conexao com o api para atualizar habitos completados

--- web/src/components/HabitsList.tsx
+++ web/src/components/HabitsList.tsx
@@ -1,4 +1,5 @@
 import * as Checkbox from '@radix-ui/react-checkbox';
+import dayjs from 'dayjs';
 import { Check } from 'phosphor-react';
 import { useEffect, useState } from 'react';
 import { api } from '../lib/axios';
@@ -13,7 +14,7 @@ interface HabitsInfo {
         title: string;
         created_at: string;
     }>,
-    completedeHabits: string[]
+    completedHabits: string[]
 }
 
 export function HabitsList({ date }: HabitsListProps) {
@@ -30,12 +31,37 @@ export function HabitsList({ date }: HabitsListProps) {
         })
     }, [])
 
+    async function handleToggleHabit(habitId: string) {
+
+        await api.patch(`/habits/${habitId}/toggle`)
+
+        const isHabitAlreadyCompleted = habitsInfo!.completedHabits.includes(habitId)
+        let completedHabits: string[] = []
+
+        if (isHabitAlreadyCompleted) {
+            completedHabits = habitsInfo!.completedHabits.filter(id => id !== habitId)
+        } else {
+            completedHabits = [...habitsInfo!.completedHabits, habitId]
+        }
+
+        setHabitsInfo({
+            possibleHabits: habitsInfo!.possibleHabits,
+            completedHabits,
+        })
+
+    }
+
+    const isDateInPast = dayjs(date).endOf("day").isBefore(new Date())
+
     return (
         <div className="mt-6 flex flex-col gap-3">
             {habitsInfo?.possibleHabits.map(habit => {
                 return (
                     <Checkbox.Root
                     key={habit.id}
+                    onCheckedChange={() => handleToggleHabit(habit.id)}
+                    checked={habitsInfo.completedHabits.includes(habit.id)}
+                    disabled={isDateInPast}
                     className="flex items-center gap-3 group"
                     >
                         <div className="h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
